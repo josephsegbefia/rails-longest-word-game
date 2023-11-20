abc6fcde089c9110119d6442b9e7b3aab74e85c9
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-20 19:50:47 +0000

    Done without styles

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,14 @@
+require "open-uri"
+
+url = ""
 class GamesController < ApplicationController
-
-
   def new
     @letters = ("A".."Z").to_a.sample(10)
   end
 
   def score
-    # @word = params[:word]
-    # print @word
-    raise
+    @letters = params[:letters]
+    @word = params[:word].upcase
+    @result = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{@word}").read)
   end
 end
